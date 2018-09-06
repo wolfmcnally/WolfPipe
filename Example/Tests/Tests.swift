2ed757d12f0d5cb1368d1bfd63eb000d1b83f1de
Wolf McNally <wolf@wolfmcnally.com>
2018-09-06 11:18:34 -0700

    Added pipe-compatible versions of sorted and map.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,19 +2,15 @@ import XCTest
 import WolfPipe
 
 class Tests: XCTestCase {
-    func test1() {
-        func addTwo(to i: Int) -> Int {
-            return i + 2
-        }
+    class TestError: Error { }
 
-        XCTAssert(2 |> addTwo == 4)
+    func testClosure() {
+        XCTAssert(2 |> { $0 + 2 } == 4)
     }
 
-    func test2() {
+    func testAppendFilter() {
         func filterOrThrow(i: Int) throws -> Bool {
-            if i > 100 {
-                throw NSError(domain: "None", code: 1)
-            }
+            if i > 100 { throw TestError() }
             return i > 2
         }
 
@@ -26,5 +22,32 @@ class Tests: XCTestCase {
         XCTAssert(c == [3, 4, 5])
         let d = try! b |> filter(filterOrThrow)
         XCTAssert(d == [3, 4, 5])
+    }
+
+    func testSorted() {
+        func compareOrThrow(a: Int, b: Int) throws -> Bool {
+            if a > 100 || b > 100 { throw TestError() }
+            return a < b
+        }
+        let a = [5, 8, 2, 7]
+        let b = a |> sorted
+        let c = a |> sorted( > )
+        let d = try! a |> sorted(compareOrThrow)
+        XCTAssert(b == [2, 5, 7, 8])
+        XCTAssert(c == [8, 7, 5, 2])
+        XCTAssert(d == [2, 5, 7, 8])
+    }
+
+    func testMap() {
+        func multiplyOrThrow(a: Int) throws -> Double {
+            if a > 100 { throw TestError() }
+            return Double(a) * 1.5
+        }
+
+        let a = [1, 2, 3, 4]
+        let b = a |> map({ Double($0) * 1.5 })
+        let c = try! a |> map(multiplyOrThrow)
+        XCTAssert(b == [1.5, 3.0, 4.5, 6.0])
+        XCTAssert(c == [1.5, 3.0, 4.5, 6.0])
     }
 }
--- WolfPipe/Classes/PipeCompatibleFunctions.swift
+++ WolfPipe/Classes/PipeCompatibleFunctions.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-public func append<T, C>(_ e: T) -> (C) -> C where C: RangeReplaceableCollection, C.Element == T {
+/// Curried version of `append`.
+public func append<E, C>(_ e: E) -> (C) -> C where C: RangeReplaceableCollection, C.Element == E {
     return { c in
         var d = c
         d.append(e)
@@ -16,14 +17,49 @@ public func append<T, C>(_ e: T) -> (C) -> C where C: RangeReplaceableCollection
     }
 }
 
-public func filter<T, C>(_ isIncluded: @escaping (T) -> Bool) -> (C) -> C where C: RangeReplaceableCollection, C.Element == T {
+/// Curried version of `filter`.
+public func filter<E, C>(_ isIncluded: @escaping (E) -> Bool) -> (C) -> C where C: RangeReplaceableCollection, C.Element == E {
     return { c in
         return c.filter(isIncluded)
     }
 }
 
-public func filter<T, C>(_ isIncluded: @escaping (T) throws -> Bool) rethrows -> (C) throws -> C where C: RangeReplaceableCollection, C.Element == T {
+/// Curried version of `filter`, taking a throwing functor.
+public func filter<E, C>(_ isIncluded: @escaping (E) throws -> Bool) rethrows -> (C) throws -> C where C: RangeReplaceableCollection, C.Element == E {
     return { c in
         return try c.filter(isIncluded)
+    }
+}
+
+/// Curried version of `sorted` for `Array` with `Comparable` elements.
+public func sorted<E>(_ a: [E]) -> [E] where E: Comparable {
+    return a.sorted()
+}
+
+/// Curried version of `sorted` for `Array`, taking a custom compare functor.
+public func sorted<E>(_ by: @escaping (E, E) -> Bool) -> ([E]) -> [E] {
+    return { a in
+        return a.sorted(by: by)
+    }
+}
+
+/// Curried version or `sorted` for `Array`, taking a custom throwing compare functor.
+public func sorted<E>(_ by: @escaping (E, E) throws -> Bool) rethrows -> ([E]) throws -> [E] {
+    return { a in
+        return try a.sorted(by: by)
+    }
+}
+
+/// Curried version of `map`.
+public func map<E1, E2, S>(_ transform: @escaping (E1) -> E2) -> (S) -> [E2] where S: Sequence, S.Element == E1 {
+    return { s in
+        return s.map(transform)
+    }
+}
+
+/// Curried version of `map`, taking a throwing functor.
+public func map<E1, E2, S>(_ transform: @escaping (E1) throws -> E2) rethrows -> (S) throws -> [E2] where S: Sequence, S.Element == E1 {
+    return { s in
+        return try s.map(transform)
     }
 }
